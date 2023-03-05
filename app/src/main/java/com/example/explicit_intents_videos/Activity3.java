6421569a447c544c6ed862aabf2db22a1836e877
NajeraNet <najeranet.com@gmail.com>
2023-03-04 19:33:14 -0600

    En activity 3 podemos llenar campos de textos y recivirlos en un text view en nuestro main activity. con esto concluimos la primera practica de intents. ;)

--- app/src/main/java/com/example/explicit_intents_videos/Activity3.java
+++ app/src/main/java/com/example/explicit_intents_videos/Activity3.java
@@ -2,15 +2,18 @@ package com.example.explicit_intents_videos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Activity3 extends AppCompatActivity {
     //
     EditText etSurName;
     Button btnSubmit;
+    Button btnCancell;
     //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +22,35 @@ public class Activity3 extends AppCompatActivity {
         //
         etSurName = findViewById(R.id.etSurName);
         btnSubmit = findViewById(R.id.btnEnviarNN);
+        btnCancell = findViewById(R.id.btnCancell);
         //
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View viewNickNameToMA) {
+                //Preguntamos si ingreso datos
+                if (etSurName.getText().toString().isEmpty()){
+                    Toast.makeText(Activity3.this, getText(R.string.rellena_los_campos),
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    // agregamos los datos a un string
+                    String surname = etSurName.getText().toString().trim();
+                    //iniciamos el intent para pasar los datos
+                    Intent intent = new Intent();
+                    //Agregamos los datos a nuestro intent antes iniciado
+                    intent.putExtra("surname", surname);
+                    //Decimos que los datos son validos enviando un resultado como OK
+                    setResult(RESULT_OK, intent);
+                    //Cerramos el activity
+                    Activity3.this.finish();
+                }
+            }
+        });
+        //
+        btnCancell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View viewCanceled) {
+                setResult(RESULT_CANCELED);
+                Activity3.this.finish();
             }
         });
     }
